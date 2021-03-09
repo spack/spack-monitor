4e2830ec2f025c02fec289d39e41f191ebb741c0
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2021-03-09 09:18:34 -0700

    adding basic build interfaces (#17)
    * adding basic build interfaces
    
    this will add a dashboard that shows a table of builds, along with
    a build information page that shows three columns with a summary,
    details of the build environment, and phase status and output.
    We likely will want to tweak this to better fit our needs when we know
    what those needs are
    
    * updating table for removed field
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>
    Co-authored-by: vsoch <vsoch@users.noreply.github.com>

--- spackmon/apps/main/views.py
+++ spackmon/apps/main/views.py
@@ -22,6 +22,11 @@ from spackmon.settings import (
 
 @ratelimit(key="ip", rate=rl_rate, block=rl_block)
 def index(request):
-    # All users can see all specs
     builds = Build.objects.all()
     return render(request, "main/index.html", {"builds": builds})
+
+
+@ratelimit(key="ip", rate=rl_rate, block=rl_block)
+def build_detail(request, bid):
+    build = get_object_or_404(Build, pk=bid)
+    return render(request, "builds/detail.html", {"build": build})
