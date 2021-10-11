2e56e0875f41a23b1c634c334315e683c6d7035b
vsoch <vsoch@users.noreply.github.com>
2021-10-11 14:06:45 -0600

    try custom entrypoint for kubernetes that uses gunicorn
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ script/k8s/run_uwsgi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# If the original migrations aren't run - do it.
+# If you want to change models and run them again, you should run 
+# these commands manually.
+if [ ! -f "/opt/migrations-run" ]; then
+    sleep 3
+    python manage.py makemigrations users
+    python manage.py makemigrations main
+    python manage.py makemigrations
+    python manage.py migrate users
+    python manage.py migrate main
+    python manage.py migrate auth
+    python manage.py migrate 
+    python manage.py collectstatic --noinput
+    touch /opt/migrations-run
+fi
+
+# Kubernetes does not work with uwsgi
+gunicorn --bind :3031 --workers 4 spackmon.wsgi
