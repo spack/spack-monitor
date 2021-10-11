8ae487e1dcab97902b677df135b812fcfca4c193
vsoch <vsoch@users.noreply.github.com>
2021-10-11 13:36:25 -0600

    remove extra print
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- spackmon/settings.py
+++ spackmon/settings.py
@@ -266,7 +266,6 @@ if running_tests or cfg.USE_SQLITE:
 
 # Database local development uses DATABASE_* variables
 elif os.getenv("DATABASE_HOST") is not None:
-    print("FOUND pre-defined DATABASE_HOST")
     # Make sure to export all of these in your .env file
     DATABASES = {
         "default": {
